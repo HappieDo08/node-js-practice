2c6e0f61fa8fe3dd7e7f90c7c5a1d26dbe123483
GStudies <aquinogerardoXIII@gmail.com>
2024-09-25 00:04:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const program = new Command();
 program
 .option("-s, --show", "Display your Information")
 .option("-n, --name <name>", "Your Name")
+.option("-a, --age <age>", "Your Name")
 .option("-g, --gender <gender>", "Your Gender")
 .allowUnknownOption(true);
 
@@ -19,12 +20,15 @@ const options = program.opts();
 
 const user = {
     name: options.name || "Unknown",
+    age: options.age || "Unknown",
     gender: options.gender || "Unknown",
+    
 };
 
 const displayUser = () => {
     console.log("User Information");
     console.log(`Name: ${user.name}`);
+    console.log(`Age: ${user.age}`);
     console.log(`Gender: ${user.gender}`);
 };
 
